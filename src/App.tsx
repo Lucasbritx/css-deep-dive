6cf4bdeabb7be0b23d2699caeb00d4dc1a5b2501
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-09 08:20:36 -0300

    presentation subjects

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,26 @@ function App() {
   return (
     <div>
       CSS DEEP DIVE
+
+      <div>
+        <h1>Cascade Layers (@layer)</h1>
+        <div></div>
+      </div>
+      <div>
+        <h1>:has() como CSS parent selector</h1>
+      </div>
+
+      <div>
+        <h1>Container Queries + @container style()</h1>
+      </div>
+
+      <div>
+        <h1>CSS Triggers e Performance</h1>
+      </div>
+
+      <div>
+        <h1>Houdini + Custom Properties com fallback</h1>
+      </div>
     </div>
   )
 }
