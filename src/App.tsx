07daf07768b7169127f4e7ab21810a6310ef6ebf
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-09 09:59:35 -0300

    cascade layers example

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
+import React from "react";
+import CascadeLayers from "./components/cascade-layers";
 
 function App() {
-
   return (
     <div>
       CSS DEEP DIVE
-
-      <div>
-        <h1>Cascade Layers (@layer)</h1>
-        <div></div>
-      </div>
+      <CascadeLayers />
       <div>
         <h1>:has() como CSS parent selector</h1>
       </div>
-
       <div>
         <h1>Container Queries + @container style()</h1>
       </div>
-
       <div>
         <h1>CSS Triggers e Performance</h1>
       </div>
-
       <div>
         <h1>Houdini + Custom Properties com fallback</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/cascade-layers/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.css'
+
+function CascadeLayers() {
+  return (
+    <div>
+      <div>
+        <h1>Cascade Layers (@layer)</h1>
+      </div>
+    </div>
+  );
+}
+
+export default CascadeLayers;
