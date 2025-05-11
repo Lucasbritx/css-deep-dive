a4721f2311a744a55e5e88c9eb997cedeb2ef3b4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-11 00:00:02 -0300

    removing unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import CascadeLayers from "./components/cascade-layers";
 
 function App() {
   return (
     <div>
       CSS DEEP DIVE
-      <CascadeLayers />
-      <div>
-        <h1>:has() como CSS parent selector</h1>
-      </div>
-      <div>
-        <h1>Container Queries + @container style()</h1>
-      </div>
-      <div>
-        <h1>CSS Triggers e Performance</h1>
-      </div>
-      <div>
-        <h1>Houdini + Custom Properties com fallback</h1>
-      </div>
     </div>
   );
 }
